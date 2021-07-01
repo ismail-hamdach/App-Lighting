dad2600773b003367be28cf2b16bc946138dda35
ismail-hamdach <ismailhamdach@gmail.com>
2021-07-01 20:55:22 +0100

    update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,23 @@
 import React, { useContext, useState } from 'react'
-import {View, Text, StyleSheet, Image, Button } from 'react-native'
+import {View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 
 import {AuthContext} from '../../providers/AuthProvider'
 
 const Home = () =>{
     const {user} = useContext(AuthContext);
     const {logOut} = useContext(AuthContext);
+    const profile = "https://firebasestorage.googleapis.com/v0/b/app-lamps.appspot.com/o/profile.png?alt=media&token=6a471065-e8fd-42af-b019-91e1dbfa4883"
+
     return(
         <View style={styles.container}>
-            <Image style={styles.image} source={{uri: user.photoURL}}/>
+            <Image style={styles.image} source={{uri: user.photoURL??profile}}/>
             <Text style={styles.title}>{user.displayName??'Name'}</Text>
-            <Button title = {"Se déconnecter"} onPress = {() => {logOut()}}/>
+            {/* <Button title = {"Se déconnecter"} onPress = {() => {logOut()}}/> */}
+            <TouchableOpacity style={styles.buttomLogOut} 
+              onPress={ () => {logOut()}}
+            >
+              <Text style={styles.verificetion}> Se déconnecter </Text>
+          </TouchableOpacity>
         </View>
     );
 }
@@ -18,8 +25,10 @@ const Home = () =>{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        position: 'relative',
+        top: 30,
+        // justifyContent: 'center',
+        // alignItems: 'center',
     },item: {
         backgroundColor: '#F0F0F0',
     
@@ -40,6 +49,21 @@ const styles = StyleSheet.create({
         borderRadius:10,
         top:17,
         left:15,
+      },
+      buttomLogOut: {
+        top: 10,
+        alignItems: 'center',
+        alignSelf: 'center',
+        width:150,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#140A7E',
+      },
+      verificetion: {
+        color: '#FFf',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top:15,
       },
 })
 
