fa7f0e53a22546d263b18907831c77987f4585e9
ismail-hamdach <ismailhamdach@gmail.com>
2021-07-05 06:47:46 +0100

    update

--- src/components/admin/DashBoard.js
+++ src/components/admin/DashBoard.js
@@ -26,36 +26,61 @@ const DashBoard = ({navigation}) => {
       }
   ]);
 
+  // Listener to the database (realTime)
   const getAnalyse = async (dataRetrieved) => {
+    
+    var db = firebase.firestore();
+    //lISTEN to change in the database
+    await db.collection("poteaux")
+    .onSnapshot((querySnapshot) => {
+      //when there is change fetch data
+        getAnalyseOnce(dataRetrieved)
+    }, (error) => {
+        console.log("Error getting documents: ", error);
+    });
+  }
+
+  // Fetching data from database
+  const getAnalyseOnce = async (dataRetrieved) => {
     const on = []
     const off = []
     var data = null;
-    const error = null
+    
+    // initialise data, data: used to store result of fetching
+    data = [
+      {
+          key: 1,
+          amount: 0,
+          name: 'Off',
+          svg: { fill: '#3d3d3d' },
+      },
+      {
+          key: 2,
+          amount: 0,
+          name: 'On',
+          svg: { fill: '#366464' }
+      }
+    ];
 
     var db = firebase.firestore();
-
+    // fetching data of poteax who is on
     await db.collection("poteaux").
     where("etat", "==", true)
     .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
             on.push(doc.data)
             console.log(doc.id, " => ", doc.data());
-            
         });
-    })
-    .catch((error) => {
+        data[1].amount = on.length;
+    }).catch((error) => {
         console.log("Error getting documents: ", error);
-        error = true;
     });
-
-    await db.collection("poteaux").
-    where("etat", "==", false)
+    // fetching data of poteax who is off
+    await db.collection("poteaux").where("etat", "==", false)
     .get()
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
             off.push(doc.data)
             console.log(doc.id, " => ", doc.data());
         });
@@ -63,29 +88,9 @@ const DashBoard = ({navigation}) => {
     })
     .catch((error) => {
         console.log("Error getting documents: ", error);
-        error = true;
     });
-
-    
-
-    data = [
-      {
-          key: 1,
-          amount: off.length??0,
-          name: 'Off',
-          svg: { fill: '#3d3d3d' },
-      },
-      {
-          key: 2,
-          amount: on.length??0,
-          name: 'On',
-          svg: { fill: '#366464' }
-      }
-    ];
     console.log(data)
     dataRetrieved(data)
-    
-
   }
 
   const Labels = ({ slices }) => {
@@ -101,29 +106,24 @@ const DashBoard = ({navigation}) => {
                 alignmentBaseline={'middle'}
                 fontSize={24}
             >
-                {data.amount == 0? "Aucune resultat trouvée" : data.amount}
+                {data.amount == 0? null : data.amount}
             </TextSVG>
         )
     })
   }
 
+  //Fetch data and lunch the listener
   const open = async () => {
-    navigation.setOptions({
-      headerShown: false
-    })
-    await getAnalyse(setData)
-    setTimeout(() =>{ 
-      
-      navigation.setOptions({
-        headerShown: true
-      })
-      setIsLoading(false)
-    }, 1000)
+    setIsLoading(true);
+    //To get data at first time to impliment loading effect
+    await getAnalyseOnce(setData);
+    //To lunch the listener
+    await getAnalyse(setData);
+    setIsLoading(false);
   }
 
   useEffect( () => {
     open()
-    
   }, [])
 
   return (isLoading ? 
@@ -131,46 +131,48 @@ const DashBoard = ({navigation}) => {
       <ActivityIndicator size="large" color="#140A7E" />
     </View>
   :
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style= {{flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-          <View style= {{backgroundColor: '#21C3A7', height: '60%', width: '70%', justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
-            <Text style={{fontWeight: 'bold', color: '#ffffff', fontSize: 24}}>
-              {data[0].amount + data[1].amount} Poteaux
-            </Text>
-          </View>
-          
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      
+      <View style= {{flex: 1, width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        
+        <View style= {{backgroundColor: '#21C3A7', height: '60%', width: '70%', justifyContent: 'center', alignItems: 'center', borderRadius: 15 }}>
+          <Text style={{fontWeight: 'bold', color: '#ffffff', fontSize: 24}}>
+            {data[0].amount + data[1].amount} Poteaux
+          </Text>
         </View>
+        
+      </View>
 
-        <View style= {{flex: 2, flexDirection: 'column', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
-          <View style={{backgroundColor: '#ffffff', justifyContent: 'center', height: '95%', width: '95%', borderRadius: 15, paddingTop: '5%', paddingBottom: '5%'}}>
-            <PieChart
-                style={{flex: 3, height: '100%' }}
-                valueAccessor={({ item }) => item.amount}
-                data={data}
-            >
-                <Labels/>
-            </PieChart>
-            <View style={{flex: 1, justifyContent: 'center', marginLeft: '3%'}}>
-              
-            <View style={{flexDirection: 'row'}}>
-                <View style={{backgroundColor: '#366464', height: 20, width: 20}}></View>
-                <Text style={{ marginLeft: '5%', fontWeight: 'bold', color: '#366464'}}>
-                  On {(data[1].amount / (data[0].amount + data[1].amount)).toFixed(4)*100}%
-                </Text>
-              </View>
-
-              <View style={{flexDirection: 'row', marginTop: '5%',}}>
-                <View style={{backgroundColor: '#3d3d3d', height: 20, width: 20}}></View>
-                <Text style={{ marginLeft: '5%', fontWeight: 'bold'}}>
-                  Off {(data[0].amount / (data[0].amount + data[1].amount)).toFixed(4)*100}%
-                </Text>
-              </View>
-              
+      <View style= {{flex: 2, flexDirection: 'column', width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{backgroundColor: '#ffffff', justifyContent: 'center', height: '95%', width: '95%', borderRadius: 15, paddingTop: '5%', paddingBottom: '5%'}}>
+          <PieChart
+              style={{flex: 3, height: '100%' }}
+              valueAccessor={({ item }) => item.amount}
+              data={data}
+          >
+              <Labels/>
+          </PieChart>
+          <View style={{flex: 1, justifyContent: 'center', marginLeft: '3%'}}>
+            
+          <View style={{flexDirection: 'row'}}>
+              <View style={{backgroundColor: '#366464', height: 20, width: 20}}></View>
+              <Text style={{ marginLeft: '5%', fontWeight: 'bold', color: '#366464'}}>
+                On {((data[1].amount / (data[0].amount + data[1].amount))*100).toFixed(2)}%
+              </Text>
             </View>
+
+            <View style={{flexDirection: 'row', marginTop: '5%',}}>
+              <View style={{backgroundColor: '#3d3d3d', height: 20, width: 20}}></View>
+              <Text style={{ marginLeft: '5%', fontWeight: 'bold'}}>
+                Off {((data[0].amount / (data[0].amount + data[1].amount))*100).toFixed(2)}%
+              </Text>
+            </View>
+            
           </View>
         </View>
       </View>
-      
+    
+    </View>
   )
 
 }
